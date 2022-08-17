228de8b5ced6fec43560d1757740c6f2062b4c36
DanK1368 <danielkoett@gmail.com>
2022-08-17 11:07:17 +0200

    Link tags added for icons and aria-labels added to button and link tags

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,16 +10,30 @@ const Navbar = () => {
         <header>
             <div className="min-h-8 pt-10 pl-4 w-1/2 flex justify-between items-center sm:w-full sm:px-4 ">
                 <nav>
-                    <FiMenu size={30} />
+                    <button>
+                        <FiMenu size={30} aria-label="Menu" />
+                    </button>
                 </nav>
                 <div className=" w-[60px] h-[60px] translate-x-[50%] ">
                     <Image src={logo} alt="sophie herter photography logo" />
                 </div>
-                <div className=" hidden sm:flex justify-center items-center gap-4">
-                    <FiInstagram />
-                    <FaFacebookF />
-                    <BsFillShareFill />
-                </div>
+                <ul className=" hidden sm:flex justify-center items-center gap-4">
+                    <li>
+                        <a href="https://www.instagram.com/" target="_blank" rel="noreferrer" >
+                            <FiInstagram aria-label="Link to Instagram Page" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+                            <FaFacebookF aria-label="Link to Facebook Page" />
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            <BsFillShareFill aria-label="Link to share website on social media" />
+                        </a>
+                    </li>
+                </ul>
             </div>
         </header>
     );
