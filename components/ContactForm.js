620f2f41c6010d7ae63ab1386c389dfbb6728132
DanK1368 <danielkoett@gmail.com>
2022-08-24 13:26:14 +0200

    added contact form sending of message functionality

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,7 +1,48 @@
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 import Image from "next/image";
 import illustration from "../public/assets/illustration.svg";
 
 const ContactForm = () => {
+  const form = useRef();
+  const [inputValues, setInputValues] = useState({
+    user_name: "",
+    user_email: "",
+    user_message: "",
+  });
+
+  const handleInputValues = e => {
+    setInputValues(prevValues => {
+      return { ...prevValues, [e.target.name]: e.target.value };
+    });
+  };
+
+  const sendEmail = e => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        `${process.env.NEXT_PUBLIC_EMAIL_JS_SERVICE_ID}`,
+        `${process.env.NEXT_PUBLIC_EMAIL_JS_TEMPLATE_ID}`,
+        form.current,
+        `${process.env.NEXT_PUBLIC_EMAIL_JS_PUBLIC_KEY}`
+      )
+      .then(
+        result => {
+          if (result.status === 200) {
+            setInputValues({
+              user_name: "",
+              user_email: "",
+              user_message: "",
+            });
+          }
+        },
+        error => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <section className="  min-h-[78vh] p-4 md:max-w-[90%] md:mx-auto 2xl:max-w-[80%]  ">
       <div className="  flex justify-center items-center flex-col ">
@@ -12,7 +53,11 @@ const ContactForm = () => {
       </div>
       <div className=" flex flex-col justify-around items-center md:flex-row gap-10 ">
         <div className=" w-full md:max-w-[600px] ">
-          <form className=" rounded-md shadow-lg shadow-slate-200 flex flex-col justify-center items-center gap-2 py-8 mt-5 ">
+          <form
+            ref={form}
+            onSubmit={sendEmail}
+            className=" rounded-md shadow-lg shadow-slate-200 flex flex-col justify-center items-center gap-2 py-8 mt-5 "
+          >
             <div className=" w-full px-3 flex flex-col justify-center items-center gap-3  ">
               <label className=" w-full ">
                 <input
@@ -20,14 +65,20 @@ const ContactForm = () => {
                   type="text"
                   placeholder="Name"
                   required
+                  name="user_name"
+                  value={inputValues.user_name}
+                  onChange={handleInputValues}
                 />
               </label>
               <label className=" w-full ">
                 <input
                   className=" w-full py-2 px-4 border-gray-300 border rounded-md tracking-wider placeholder:font-light placeholder:text-gray-500 focus:outline-none focus:border focus:border-gray-900 "
                   type="email"
+                  name="user_email"
+                  value={inputValues.user_email}
                   placeholder="Email"
                   required
+                  onChange={handleInputValues}
                 />
               </label>
               <label className=" w-full ">
@@ -35,11 +86,18 @@ const ContactForm = () => {
                   className=" w-full py-2 px-4 border-gray-300 border rounded-md tracking-wider placeholder:font-light placeholder:text-gray-500 focus:outline-none focus:border focus:border-gray-900 "
                   cols="30"
                   rows="10"
+                  name="user_message"
+                  required
+                  value={inputValues.user_message}
                   placeholder="Your Message"
+                  onChange={handleInputValues}
                 ></textarea>
               </label>
             </div>
-            <button className=" btn btn-ghost border border-gray-300 ">
+            <button
+              type="submit"
+              className=" btn btn-ghost border border-gray-300 "
+            >
               Send Message
             </button>
           </form>
